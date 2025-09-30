268538f73f97546b684259c2745540fc5fe28520
BohdanIpy <bohdanipy@mailfence.com>
2025-09-30 20:47:56 +0200

    Finished the net routines function

--- cmd/concurentTextProcessingPipeline/main.go
+++ cmd/concurentTextProcessingPipeline/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"github.com/BohdanIpy/concurentTextProcessingPipeline/internal/netRoutines"
+	"time"
+)
 
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*3)
+	defer cancel()
+	for word := range netRoutines.FetchWord(ctx) {
+		fmt.Println(word)
+	}
 }
--- internal/netRoutines/netRoutines.go
+++ internal/netRoutines/netRoutines.go
@@ -2,37 +2,49 @@ package netRoutines
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
-func fetchWord(ctx context.Context) <-chan string {
+func FetchWord(ctx context.Context) <-chan string {
 	words := make(chan string, 2)
 	go func() {
 		defer close(words)
+
+		url := "https://random-word.ryanrk.com/api/en/word/random"
+
 		for {
 
-			resp, err := http.Get("https://random-word.ryanrk.com/api/en/word/random")
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+
+			req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 			if err != nil {
-				log.Fatal(err)
+				log.Println("Request build error: ", err)
+				return
+			}
+
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				log.Println("Request build error: ", err)
 				return
 			}
 
 			body, _ := io.ReadAll(resp.Body)
-			fmt.Println(string(body))
-			err = resp.Body.Close()
-			if err != nil {
-				log.Println(err)
-				continue
-			}
+			_ = resp.Body.Close()
 
 			select {
 			case <-ctx.Done():
 				return
 			case words <- string(body):
+				log.Printf("Fetched word, written into chanel: %s", string(body))
 			}
+			time.Sleep(500 * time.Millisecond)
 		}
 	}()
 	return words
