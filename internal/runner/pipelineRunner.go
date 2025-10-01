bf175b65a6e08b2cfba576c5622260932af661ee
BohdanIpy <bohdanipy@mailfence.com>
2025-10-02 00:16:33 +0200

    removed comments

--- internal/runner/pipelineRunner.go
+++ internal/runner/pipelineRunner.go
@@ -9,14 +9,12 @@ import (
 )
 
 func Run(ctx context.Context, urls []string, takes int) <-chan string {
-	// Fetch + sentence generator
 	channelsFetchedSentences := make([]<-chan string, len(urls))
 	for i, url := range urls {
 		channelsFetchedSentences[i] = pipeline.GenerateSentences(ctx, netRoutines.FetchWord(ctx, url))
 	}
 	channelFannedSentences := fanInOut.FanIn(ctx, channelsFetchedSentences...)
 
-	// Split stage (fan-out)
 	numCPU := runtime.NumCPU()
 	channelsSplit := make([]<-chan string, numCPU)
 	for i := 0; i < numCPU; i++ {
@@ -24,13 +22,11 @@ func Run(ctx context.Context, urls []string, takes int) <-chan string {
 	}
 	channelFannedWords := fanInOut.FanIn(ctx, channelsSplit...)
 
-	// Filter stage (fan-out)
 	channelsFilteringWords := make([]<-chan string, numCPU)
 	for i := 0; i < numCPU; i++ {
 		channelsFilteringWords[i] = pipeline.FilterWords(ctx, channelFannedWords, 3)
 	}
 	channelWordsForTaken := fanInOut.FanIn(ctx, channelsFilteringWords...)
 
-	// Take stage
 	return pipeline.TakeWords(ctx, channelWordsForTaken, takes)
 }
