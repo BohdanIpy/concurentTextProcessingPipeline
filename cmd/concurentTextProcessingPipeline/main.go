862d76d0452ed91e6ad1316055d26d06f5cfb967
BohdanIpy <bohdanipy@mailfence.com>
2025-10-02 00:08:12 +0200

    Added the example of main logic

--- cmd/concurentTextProcessingPipeline/main.go
+++ cmd/concurentTextProcessingPipeline/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/BohdanIpy/concurentTextProcessingPipeline/internal/fanInOut"
 	"github.com/BohdanIpy/concurentTextProcessingPipeline/internal/netRoutines"
+	"github.com/BohdanIpy/concurentTextProcessingPipeline/internal/pipeline"
 	"runtime"
 	"time"
 )
@@ -18,12 +20,27 @@ func main() {
 		"https://random-words-api.kushcreates.com/api?language=en&words=1",
 	}
 
-	for _, url := range urls {
-		go func() {
-			for word := range netRoutines.FetchWord(ctx, url) {
-				fmt.Println(word)
-			}
-		}()
+	channelsFetchedSentences := make([]<-chan string, len(urls))
+	for i, url := range urls {
+		channelsFetchedSentences[i] = pipeline.GenerateSentences(ctx, netRoutines.FetchWord(ctx, url))
+	}
+	channelFannedSentences := fanInOut.FanIn(ctx, channelsFetchedSentences...)
+
+	numCPU := runtime.NumCPU()
+	channelsSplit := make([]<-chan string, numCPU)
+	for i := 0; i < numCPU; i++ {
+		channelsSplit[i] = pipeline.SplitSentences(ctx, channelFannedSentences)
+	}
+	channelFannedWords := fanInOut.FanIn(ctx, channelsFetchedSentences...)
+
+	channelsFilteringWords := make([]<-chan string, numCPU)
+	for i := 0; i < numCPU; i++ {
+		channelsFilteringWords[i] = pipeline.FilterWords(ctx, channelFannedWords, 3)
+	}
+
+	channelWordsForTaken := fanInOut.FanIn(ctx, channelsFilteringWords...)
+	for word := range pipeline.TakeWords(ctx, channelWordsForTaken, 5) {
+		fmt.Println(word)
 	}
 
 	time.Sleep(time.Second * 10)
